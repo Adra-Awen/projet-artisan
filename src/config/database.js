d39e6de0dceb5bccee5520a86c4a96087fc00071
Nolwenn Bauvais <bauvais.n@protonmail.com>
2026-04-09 13:11:03 +0200

    fix database

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 3306,
-        dialectModule: 'mysql',
+        dialect: 'mysql',
         logging: false
     }
 );
